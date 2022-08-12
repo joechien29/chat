75db0f76f8e5028232719e7c99ef574b6961223c
joechien29 <76095375+joechien29@users.noreply.github.com>
2022-08-12 13:52:37 +0800

    fix error'

--- r3.py
+++ r3.py
@@ -1,16 +1,17 @@
 lines = []
-with open("[Line]Ruby.txt", "r", encoding="utf-8") as f:
+with open("[Line]time_Ruby.txt", "r", encoding="utf-8") as f:
     for line in f:
         lines.append(line)
 
-convert_line = []
+convert_msg = []
 for line in lines:
     line = line.strip().split(" ")
     time = line[0][:5]
     name = line[0][5:]
-    chat = line[1]
-    convert_line.append(time + " " + name + ":" + chat)
+    msg = line[1]
+    convert_msg.append(time + " " + name + ": " + msg)
 
 with open("r3_output.txt", "w") as f:
-    for line in convert_line:
+    for line in convert_msg:
         f.write(line + "\n")
+
